f93b98a00287548fcde59fbdf266740b08d2efaf
Bontu-Andrei <60428831+bontu-andrei@users.noreply.github.com>
2020-02-05 14:55:36 +0200

    4, index.php, http://pm.5psolutions.net/public/index.php?path_info=projects/training-andrei-bontu/tasks/4, Fixing issues, Making new functions in common, Used another way to implement html template, Added image feature, Updated query

--- public/common.php
+++ public/common.php
@@ -4,10 +4,14 @@ require_once "config.php";
 
 function pdoConnectMysql() {
     try {
-        return new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASSWORD);
+        return new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PASSWORD);
     } catch (PDOException $exception) {
-        die ('Failed to connect to database!');
+        die ("Failed to connect to database!");
     }
+}
+
+function session() {
+    return session_start();
 }
 
 function trans($name) {
@@ -18,7 +22,7 @@ function getAllProductsFromCart() {
     $pdo = pdoConnectMysql();
 
     //List products from cart
-    $productsInCart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
+    $productsInCart = isset($_SESSION["cart"]) ? $_SESSION["cart"] : array();
     $products = [];
 
     if (count($productsInCart) > 0) {
@@ -35,40 +39,10 @@ function getAllProductsFromCart() {
     return $products;
 }
 
-function template_header($title) {
-    echo <<<EOT
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>$title</title>
-    </head>
-    <body>
-        <header style="background-color: #9e9e9eb5">
-            <div>
-                <h1 style="text-align: center">Training</h1>
-                <nav>
-                    <h4><a href="index.php">Index</a></h4>
-                    <h4><a href="cart.view.php">Cart</a></h4>
-                    <h4><a href="products.php">Products</a></h4>
-                </nav>
-            </div>
-        </header>
-        <main>
-EOT;
+function getImagePath($product) {
+    if ($product["image_path"]) {
+        return "/images/" . $product["image_path"];
+    } else {
+        return "/images/default.jpg"; }
 }
-// Template footer
-function template_footer() {
-    $year = date('Y');
-    echo <<<EOT
-        </main>
-        <footer>
-            <div style="text-align: center">
-                <p>&copy; $year, Training</p>
-            </div>
-        </footer>
-    </body>
-</html>
-EOT;
-}
-?>
+
--- public/index.php
+++ public/index.php
@@ -1,68 +1,76 @@
 <?php
+require_once "common.php";
 
-    session_start();
+$session = session();
 
-    require_once "common.php";
+$pdo = pdoConnectMysql();
 
-    $pdo = pdoConnectMysql();
+$productsFromCart = getAllProductsFromCart();
 
-    $stmt = $pdo->prepare("SELECT * FROM products LIMIT 500");
-    $stmt->execute();
-    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$idsProductsNotInCart = [];
 
-    if ( ! array_key_exists("cart", $_SESSION)) {
-        $_SESSION["cart"] = [];
-    }
+foreach ($productsFromCart as $productFromCart) {
+    $idsProductsNotInCart = (int)$productFromCart["id"];
+}
 
-    $productsNotInCart = [];
+$stmt = $pdo->prepare("SELECT * FROM products WHERE id NOT IN ($idsProductsNotInCart)");
+$stmt->execute();
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    if (count($_SESSION["cart"]) !== 0) {
-        foreach ($products as $index => $productInDB) {
-            $existsInCart = false;
+if (!array_key_exists("cart", $_SESSION)) {
+    $_SESSION["cart"] = [];
+}
 
-            foreach ($_SESSION["cart"] as $productId) {
-                if ((int) $productId === (int) $productInDB['id']) {
-                    $existsInCart = true;
-                }
-            }
+$productsNotInCart = [];
 
-            if ( ! $existsInCart) {
-                $productsNotInCart[] = $productInDB;
+if (count($_SESSION["cart"]) !== 0) {
+    foreach ($products as $index => $productInDB) {
+        $existsInCart = false;
+
+        foreach ($_SESSION["cart"] as $productId) {
+            if ((int)$productId === (int)$productInDB["id"]) {
+                $existsInCart = true;
             }
         }
-    } else {
-        $productsNotInCart = $products;
-    }
 
+        if (!$existsInCart) {
+            $productsNotInCart[] = $productInDB;
+        }
+    }
+} else {
+    $productsNotInCart = $products;
+}
 ?>
 
+<?php include "includes/header.php"; ?>
 
-<?=template_header("Home")?>
+<h2><?= trans("Products who does not exist in the cart") ?></h2>
 
-    <h2>Products who does not exist in the cart</h2>
-
-    <?php foreach ($productsNotInCart as $productNotInCart) : ?>
-        <div style="border: 1px solid black; width: 600px; height: 120px; margin: 10px; display: flex;
+<?php foreach ($productsNotInCart as $productNotInCart) : ?>
+    <div style="border: 1px solid black; width: 600px; height: 120px; margin: 10px; display: flex;
                                  align-items: center; justify-content: space-evenly;">
 
-            <img src="images/default.jpg" alt="product_image" style="width: 100px; height: 100px;">
+        <img src="<?= getImagePath($productNotInCart) ?>" alt="<?= trans("product_image") ?>"
+             style="width: 100px; height: 100px;">
 
-            <div>
-                <label for="title"><b><?= trans("Title:") ?></b></label>
-                <span name="title"><?= $productNotInCart["title"] ?></span> <br>
+        <div>
+            <label for="title"><b><?= trans("Title:") ?></b></label>
+            <span name="title"><?= $productNotInCart["title"] ?></span> <br>
 
-                <label for="description"><b><?= trans("Description:") ?></b></label>
-                <span name="description"><?= $productNotInCart["description"] ?></span> <br>
+            <label for="description"><b><?= trans("Description:") ?></b></label>
+            <span name="description"><?= $productNotInCart["description"] ?></span> <br>
 
-                <label for="price"><b><?= trans("Price:") ?></b></label>
-                <span name="price"><?= $productNotInCart["price"] ?></span> <br>
-            </div>
-
-            <form action="cart.php" method="POST">
-                <input type="hidden" value="<?= $productNotInCart["id"] ?>" name="product_id">
-                <button type="submit">Add to cart</button>
-            </form>
+            <label for="price"><b><?= trans("Price:") ?></b></label>
+            <span name="price"><?= $productNotInCart["price"] ?></span> <br>
         </div>
-    <?php endforeach; ?>
 
-<?=template_footer("")?>
+        <form action="cart.php" method="POST">
+            <input type="hidden" value="<?= $productNotInCart["id"] ?>" name="product_id">
+            <button type="submit">Add to cart</button>
+        </form>
+    </div>
+<?php endforeach; ?>
+
+<?php include "includes/footer.php"; ?>
+
+

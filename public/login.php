e0e8f5959831794b30b5d68c7cca5975e8db7e0e
Bontu-Andrei <60428831+bontu-andrei@users.noreply.github.com>
2020-02-13 10:10:51 +0200

    6, login.php, http://pm.5psolutions.net/public/index.php?path_info=projects/training-andrei-bontu/tasks/6, Implementing validation, Deleting login-check.php file

--- public/login.php
+++ public/login.php
@@ -2,6 +2,36 @@
 
 require_once 'common.php';
 
+if (isset($_POST['user']) && isset($_POST['password'])) {
+    $errors = [
+        'user' => '',
+        'password' => '',
+    ];
+
+    if (!validateRequiredInput('user')) {
+        $errors['user'] = 'User field is required.';
+    }
+
+    if (!validateRequiredInput('password')) {
+        $errors['password'] = 'Password field is required.';
+    }
+
+    if (!$errors['user'] && !$errors['password']) {
+        $errors = [
+            'user' => '',
+            'password' => '',
+        ];
+
+        if (strip_tags($_POST['user']) === ADMIN_USERNAME && strip_tags($_POST['password']) === ADMIN_PASSWORD) {
+            // logged in
+            $_SESSION['logged_in'] = true;
+
+            header('Location: products.php');
+            exit();
+        }
+    }
+}
+
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
     header('Location: index.php');
     exit();
@@ -9,12 +39,25 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
 
 ?>
 
-<form action="/login-check.php" method="POST">
+<form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST" style="display: grid; justify-content: center;">
     <h3><?= trans('Login'); ?></h3>
 
-    <input type="text" name="user" value="" placeholder="<?= trans('Enter name'); ?>">
+    <input type="text" name="user" value="<?= isset($_POST['user']) ? $_POST['user'] : ''; ?>"
+           placeholder="<?= trans('Enter name'); ?>">
 
-    <input type="password" name="password" value="" placeholder="<?= trans('Enter password'); ?>">
+    <div style="color: red;">
+        <?= isset($errors['user']) ? $errors['user'] : ''; ?>
+    </div>
 
+    <br>
+
+    <input type="password" name="password" value="<?= isset($_POST['password']) ? $_POST['password'] : ''; ?>"
+           placeholder="<?= trans('Enter password'); ?>">
+
+    <div style="color: red;">
+        <?= isset($errors['password']) ? $errors['password'] : ''; ?>
+    </div>
+
+    <br>
     <button type="submit"><?= trans('Login'); ?></button>
 </form>
